b118ee0422c5143061e84eae8ea60acd8933b360
Kihong Heo <kheo@cis.upenn.edu>
2018-10-16 13:48:25 -0400

    update

--- js/publication.js
+++ js/publication.js
@@ -22,7 +22,7 @@ var pubs = [
     venue: '<a href="https://www.sigsac.org/ccs/CCS2018">CCS 2018: ACM Conference on Computer and Communications Security</a>',
     year: 2018,
     paper: "paper/ccs18.pdf",
-    toappear: "(to appear)"
+    code: "https://github.com/aspire-project/chisel"
   },
   {
     title: "User-Guided Program Reasoning using Bayesian Inference",
